c64dc107909aaa3764885c5d89876f805d4ccbdd
bublick <k@lis.im>
2021-10-09 17:02:19 +0300

    Fake API upd and added components users + userlist add

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import User from "../user/user";
-import UserList from "../userList/userList";
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import User from '../user/user'
+import UserList from '../userList/userList'
 
 const Users = () => {
-  const { userId } = useParams();
+    const { userId } = useParams()
 
-  return <>{userId ? <User /> : <UserList />}</>;
-};
+    return <>{userId ? <User id={userId} /> : <UserList />}</>
+}
 
-export default Users;
+export default Users
