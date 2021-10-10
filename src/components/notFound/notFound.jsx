d4da03d7b6c3d8fa597c97c7a700817f2045a41f
bublick <k@lis.im>
2021-10-10 21:21:54 +0300

    Fix main

--- src/components/notFound/notFound.jsx
+++ src/components/notFound/notFound.jsx
@@ -1,20 +1,21 @@
-
+import Breadcrumbs from '../breadcrumbs/breadcrumbs'
 import query from 'query-string'
-import NavBar from "../navBar/navBar";
-const NotFound = ({crumbs, location}) => {
+import NavBar from '../navBar/navBar'
+const NotFound = ({ crumbs, location }) => {
+    //логика отображения списка избранных
+    const search = query.parse(location.search)
+    if (search?.favorites === 'true') {
+        console.log('отображаем избранных')
+    }
 
-//логика отображения списка избранных
-const search = query.parse(location.search)
-  if(search?.favorites === 'true'){
-    console.log('отображаем избранных')
-  }
-
-  //пример простого рендеринга крошек
-  return <>
-    {crumbs && <Breadcrumbs crumbs={crumbs} specify={'Ошибка'} />}
-    <h1>Страница не найдена</h1>
-  </>;
-};
+    //пример простого рендеринга крошек
+    return (
+        <>
+            {crumbs && <Breadcrumbs crumbs={crumbs} specify={'Ошибка'} />}
+            <h1>Страница не найдена</h1>
+        </>
+    )
+}
 //пример с передачей специфической последней крошки (заменяем её юзером)
 //{crumbs && <Breadcrumbs crumbs={crumbs} specify={'Александр'} />}
 
@@ -28,4 +29,4 @@ const search = query.parse(location.search)
 //для замены последней крошки (член команды меняем на имя пользователя) - нужно передать пропсом в крошки имя пользователя
 //и при рендере крошек заменить последний элемент массива (член команды) на имя пользователя
 
-export default NotFound;
+export default NotFound
