4398f7d6d37d73d70d039ad74b8424d585b3194d
bublick <k@lis.im>
2021-10-10 23:45:40 +0300

    final

--- src/components/user/user.jsx
+++ src/components/user/user.jsx
@@ -1,57 +1,108 @@
-import React, { useEffect, useState } from "react";
-import api from "../../API";
+import { array } from 'prop-types'
+import React, { useEffect, useState } from 'react'
+import { fav, unFav } from '../../utils/icons'
+import api from '../../API'
 
 const User = ({ id }) => {
-  const [user, setUser] = useState();
-  useEffect(() => {
-    api.users.getUserById(id).then(data => setUser(data));
-  }, []);
+    const [user, setUser] = useState()
+    useEffect(() => {
+        api.users.getUserById(id).then((data) => setUser(data))
+    }, [])
 
-  const handleSaveToLocal = id => {
-    localStorage.setItem("favorites", id);
-  };
+    const handleSaveToLocal = (id) => {
+        let storedNames = []
 
-  return (
-    <div className="container">
-      {user && (
-        <div className="single-user single-user__row">
-          <div className="single-user__photo-wrapper">
-            <img className="single-user__photo-wrapper" alt="" src={user.photo} />
-          </div>
+        if (localStorage.favorites) {
+            storedNames = JSON.parse(localStorage.favorites)
+        }
 
-          <div className="single-user__meta">
-            <h1 className="single-user__meta-name">{user.name}</h1>
-            <div className="single-user__meta-age">
-              <span className="single-user__meta-label">Возраст:</span>
-              {user.age}
-            </div>
-            <div className="single-user__meta-about">
-              <span className="single-user__meta-label">О себе:</span>
-              {user.about}
-            </div>
+        if (!storedNames) {
+            storedNames = []
+        }
 
-            <div className="single-user__meta-socials">
-              <span className="single-user__meta-label">Социальные сети:</span>
-              <ul className="meta-social__list">
-                {Object.keys(user.social).map(s => (
-                  <div key={s} className={"meta-social__list-item social-" + s}>
-                    <a href={user.social[s]}></a>
-                  </div>
-                ))}
-              </ul>
-            </div>
+        if (storedNames.includes(id)) {
+            storedNames = storedNames.filter((storedName) => storedName !== id)
+            localStorage.setItem('favorites', JSON.stringify(storedNames))
+        } else {
+            storedNames.push(id)
+            storedNames = Array.from(new Set(storedNames))
+        }
 
-            <div className="single-user__meta-role">
-              <span className="single-user__meta-label">Роль:</span>
-              {user.role.name}
-            </div>
+        localStorage.setItem('favorites', JSON.stringify(storedNames))
+    }
 
-            <button onClick={() => handleSaveToLocal(user._id)}>Fav</button>
-          </div>
+    const userIsFavorite = (id) => {
+        if (localStorage.favorites) {
+            let storedNames = JSON.parse(localStorage.favorites)
+
+            if (storedNames.includes(id)) {
+                return true
+            }
+        }
+    }
+
+    return (
+        <div className='container'>
+            {user && (
+                <div className='single-user single-user__row'>
+                    <div className='single-user__photo-wrapper'>
+                        <img
+                            className='single-user__photo-wrapper'
+                            alt=''
+                            src={user.photo}
+                        />
+                    </div>
+
+                    <div className='single-user__meta'>
+                        <h1 className='single-user__meta-name'>{user.name}</h1>
+                        <div className='single-user__meta-age'>
+                            <span className='single-user__meta-label'>
+                                Возраст:
+                            </span>
+                            {user.age}
+                        </div>
+                        <div className='single-user__meta-about'>
+                            <span className='single-user__meta-label'>
+                                О себе:
+                            </span>
+                            {user.about}
+                        </div>
+
+                        <div className='single-user__meta-socials'>
+                            <span className='single-user__meta-label'>
+                                Социальные сети:
+                            </span>
+                            <ul className='meta-social__list'>
+                                {Object.keys(user.social).map((s) => (
+                                    <div
+                                        key={s}
+                                        className={
+                                            'meta-social__list-item social-' + s
+                                        }
+                                    >
+                                        <a href={user.social[s]}></a>
+                                    </div>
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div className='single-user__meta-role'>
+                            <span className='single-user__meta-label'>
+                                Роль:
+                            </span>
+                            {user.role.name}
+                        </div>
+
+                        <button onClick={() => handleSaveToLocal(user._id)}>
+                            {userIsFavorite(user._id)
+                                ? 'Удалить из избранного'
+                                : 'Добавить в избранное'}
+                        </button>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
-};
+    )
+}
 
-export default User;
+export default User
--- src/components/userList/userList.jsx
+++ src/components/userList/userList.jsx
@@ -1,24 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const UsersList = ({ users }) => {
-  return (
-    <div className="container">
-      <h1>Команда</h1>
-      <div className="users-list">
-        {users
-          ? users.map(user => (
-              <Link key={user._id} className="users-list__user" to={`/users/${user._id}`}>
-                <div className="user-list__photo-wrapper">
-                  <img className="user-list__photo-img" alt="" src={user.photo} />
-                </div>
-                <div className="user-list__name">{user.name}</div>
-              </Link>
-            ))
-          : null}
-      </div>
-    </div>
-  );
-};
+    const [favUsers, setFavUsers] = useState()
 
-export default UsersList;
+    return (
+        <div className='container'>
+            <h1>Команда</h1>
+            <div className='users-list'>
+                {users
+                    ? users.map((user) => (
+                          <Link
+                              key={user._id}
+                              className='users-list__user'
+                              to={`/users/${user._id}`}
+                          >
+                              <div className='user-list__photo-wrapper'>
+                                  <img
+                                      className='user-list__photo-img'
+                                      alt=''
+                                      src={user.photo}
+                                  />
+                              </div>
+                              <div className='user-list__name'>{user.name}</div>
+                          </Link>
+                      ))
+                    : null}
+            </div>
+        </div>
+    )
+}
+
+export default UsersList
